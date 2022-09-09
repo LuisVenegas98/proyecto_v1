19b0ec0a3ba55bc5108d8d7c2d8b55ff06ba957d
LuisVenegas98 <enrique.venegasvllrr@uanl.edu.mx>
2022-09-08 22:21:51 -0500

    1er Avance_v1.4
    Teminado con los requisitos de la rubrica y decorado bonito :3

--- proyecto1/proyecto1/proyecto1.cpp
+++ proyecto1/proyecto1/proyecto1.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string>
 #include <windows.h> //Segun con esto puedo hacer lineas de distintos colores. Edit: pos si
+#include <iomanip>
 
 using namespace std;
 
@@ -19,26 +20,41 @@ string** listStr = new string*[listIntFl];
 void agregar()
 {
     b = true;
+    int c = 0;
     do
     {
+        system("cls");
         listInt[x][0] = x + 1; //Numero del articulo
 
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
         cout << "Ingrese el nombre del juego:\n";       //Nombre del videojuego
         cin.ignore();
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
         getline(cin, listStr[x][0]);
+        c = listStr[x][0].length();
+        if (c > 25)
+        {
+            listStr[x][0].erase(listStr[x][0].begin() + 25, listStr[x][0].end());
+        }
+        
 
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
         cout << "\nIngrese el a\xA4o de lanzamineto:\n";  //Año de lanzamiento
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
         cin >> listInt[x][1];
 
         do
         {
-            cout << "\nClasificacion del juego\n" <<      //clasificacion
-                "[1]. Todos (E)\n" <<
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+            cout << "\nClasificacion del juego\n";     //clasificacion
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
+            cout << "[1]. Todos (E)\n" <<
                 "[2]. Todos +10 (E10+)\n" <<
                 "[3]. Adolescentes (T)\n" <<
                 "[4]. Maduro +17 (M)\n" <<
                 "[5]. Solo Adultos +18 (A)\n" <<
                 "[6]. Aun sin calificar (RP)\n";
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
             cin >> cl;
             switch (cl)
             {
@@ -73,40 +89,72 @@ void agregar()
                     break;
                 }
                 default:
-                    cout << "Error: Esa no es una opcion";
+                {
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+                    cout << "Error: Esa no es una opcion\n";
                     break;
+                }  
             }
         } while (cl <= 0 && cl >= 7);
         
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
         cout << "\nCaracteristicas del juego:\n";       //Escribir caracteristicas
-        //cin >> listStr[x][2];
         cin.ignore();
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
         getline(cin, listStr[x][2]);
+        c = listStr[x][2].length();
+        if (c > 40)
+        {
+            listStr[x][2].erase(listStr[x][2].begin() + 40, listStr[x][2].end());
+        }
 
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
         cout << "\nDescripcion del juego:\n";           //Escribir descripcion
-        //cin >> listStr[x][3];
         cin.ignore();
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
         getline(cin, listStr[x][3]);
+        c = listStr[x][3].length();
+        if (c > 50)
+        {
+            listStr[x][3].erase(listStr[x][3].begin() + 50, listStr[x][3].end());
+        }
 
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
         cout << "\nGenero del juego:\n";                //Genero del juego
-        cin >> listStr[x][4];
+        cin.ignore();
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        getline(cin, listStr[x][4]);
+        c = listStr[x][4].length();
+        if (c > 30)
+        {
+            listStr[x][4].erase(listStr[x][4].begin() + 30, listStr[x][4].end());
+        }
 
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
         cout << "\nIngrese el precio unitario:\n";      //Valor del juego
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
         cin >> listFl[x][0];
         
         listFl[x][1] = listFl[x][0] * .16;            //Cuanto es de impuestos
         listFl[x][2] = listFl[x][0] + listFl[x][1];   //Total con impuestos
         
+        system("cls");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+        cout << "Juego agregado con exito! \1\n";
         do 
         {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
             cout << "Desea agregar otro juego? si[s] no[n]\n";
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
             cin >> sn;
 
             sn.erase(sn.begin() + 1, sn.end());
 
             if (sn != "s" && sn != "n")
             {
-                cout << "Error: esa no es una opcion\n";
+                system("cls");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+                cout << "Error: esa no es una opcion.\n";
             }
 
         } while (sn != "s" && sn != "n");
@@ -147,53 +195,54 @@ void lista()
     if (b == false) // validar si hay registros
     {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-        cout << "Aun no hay juegos registrados.\n" << "Presiona enter para continuar...";
+        cout << "Aun no hay juegos registrados. " << "Presiona enter para continuar...";
         return;
     }
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    cout << "No.\t\t";
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
-    cout << "Nombre del videojuego\t\t";
+    cout << " No. \xB3 ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    cout << "Nombre del videojuego     \xB3 ";
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    cout << "A\xA4o de lanzamiento\t\t";
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
-    cout << "Clasificacion\t\t";
+    cout << "A\xA4o de lanza. \xB3 ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    cout << "Clasf \xB3 ";
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    cout << "Caracteristicas\t\t";
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
-    cout << "Descripcion\t\t";
+    cout << left << setw(40) << "Caracteristicas" << setfill(' ') << " \xB3 ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    cout << left << setw(50) << "Descripcion" << setfill(' ') << " \xB3 ";
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    cout << "Genero\t\t";
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
-    cout << "Precio unitario\t\t";
+    cout << left << setw(30) << "Genero" << setfill(' ') << " \xB3 ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    cout << "Precio  \xB3 ";
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    cout << "Impuesto (16\x25)\t\t";
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
-    cout << "Total\t\t\n\n";
+    cout << "Impuesto (16\x25)  \xB3 ";
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    cout << "Total    \xB3 \n\n\n";
+
 
     for (int i = 0; i < x; i++)
     {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
-        cout << listInt[i][0] << " \xB3 "; //Numero del articulo
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
-        cout << listStr[i][0] << " \xB3 "; //Nombre del videojuego
+        cout << " " << left << setw(3) << listInt[i][0] << setfill(' ') << " \xB3 "; //Numero del articulo
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+        cout << left << setw(25) << listStr[i][0] << setfill(' ') << " \xB3 "; //Nombre del videojuego
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
-        cout << listInt[i][1] << " \xB3 "; //Año de lanzamiento
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
-        cout << listStr[i][1] << " \xB3 "; //Escribir caracteristicas
+        cout << left << setw(13) << listInt[i][1] << setfill(' ') << " \xB3 "; //Año de lanzamiento
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+        cout << left << setw(5) << listStr[i][1] << setfill(' ') << " \xB3 "; //Clasificacion
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
-        cout << listStr[i][2] << " \xB3 "; //Escribir descripcion
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
-        cout << listStr[i][3] << " \xB3 "; //Genero del juego
+        cout << left << setw(40) << listStr[i][2] << setfill(' ') << " \xB3 "; //Caracteristicas
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+        cout << left << setw(50) << listStr[i][3] << setfill(' ') << " \xB3 "; //Descripcion
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
-        cout << listStr[i][4] << " \xB3 "; //Valor del juego
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
-        cout << listFl[i][0] << " \xB3 ";  //Precio unitario
+        cout << left << setw(30) << listStr[i][4] << setfill(' ') << " \xB3 "; //Genero del juego
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+        cout << left << "$" << setw(6) << listFl[i][0] << setfill(' ') << " \xB3 ";  //Precio unitario
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
-        cout << listFl[i][1] << " \xB3 ";  //Impuestos
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
-        cout << listFl[i][2];              //Total con impuestos
-        cout << "\n";
+        cout << left << "$" << setw(14) << listFl[i][1] << setfill(' ') << " \xB3 ";  //Impuestos
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+        cout << left << "$" << setw(7) << listFl[i][2] << setfill(' ') << " \xB3 ";  //Total con impuestos
+        cout << "\n\n";
     }
 }
 
@@ -204,7 +253,7 @@ void main()
     listFl[0] = new float[3];
     listStr[0] = new string[5];
     
-
+    
     do
     {
         system("cls");
@@ -226,7 +275,6 @@ void main()
         {
         case 1:
         {
-            system("cls");
             agregar();
             break;
         }
@@ -260,6 +308,12 @@ void main()
         case 6:
         {
             cout << "Saliendo del sistema";
+            for (int i = 0; i < 3; i++)
+            {
+                Sleep(700);
+                cout << ". ";     
+            }
+            Sleep(400);
             break;
         }
         default:
@@ -273,5 +327,4 @@ void main()
 
 
     } while (menu != 6);
-    system("pause>null");
 }
